e986c0308afeab6787d14dbcbd55887eef34ce13
FrentonGod <113124622+FrentonGod@users.noreply.github.com>
2023-03-30 10:02:39 -0600

    Inicialización de Proyecto - ShoppingList

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+function App(){
+
+  return(
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+          <h1>Shopping list</h1>
+      <hr   />
+      </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-8">Nombre Items</div>     "Nombre"
+        <div className="col-3">Botones</div>           "Botones"
+      </div>
+        <div className="row">
+        <div className="col"></div>
+        <div className="col-8">Nombre Items</div>     "Nombre"
+        <div className="col-3">Botones</div>           "Botones"
+      </div>
+        <div className="row">
+        <div className="col"></div>
+        <div className="col-8">Nombre Items</div>     "Nombre"
+        <div className="col-3">Botones</div>           "Botones"
+          </div>
+          </div>     
+  )
+}
+
+export default App;
