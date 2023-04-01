45d536e6db6430b93a6b70613e74e680c97b8ff1
FrentonGod <113124622+FrentonGod@users.noreply.github.com>
2023-03-31 23:10:37 -0600

    Agregar botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,74 @@
-function App(){
+function App() {
 
-  return(
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">
-          <h1>Shopping list</h1>
-      <hr   />
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col text-start">
+          <h1>Shopping List</h1>
+        </div>
+        <div className="col text-end">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-plus-circle"></i>
+          </button>
+        </div>
       </div>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">1 kg</div>
+        <div className="col-5 col-md-6 text-start">Tortillas</div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="group">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
       <div className="row">
-        <div className="col"></div>
-        <div className="col-8">Nombre Items</div>     "Nombre"
-        <div className="col-3">Botones</div>           "Botones"
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">1 lt</div>
+        <div className="col-5 col-md-6 text-start">Aceite</div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="group">
+        <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
-        <div className="row">
-        <div className="col"></div>
-        <div className="col-8">Nombre Items</div>     "Nombre"
-        <div className="col-3">Botones</div>           "Botones"
+      <div className="row">
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">3 lts</div>
+        <div className="col-5 col-md-6 text-start">Refresco de cola</div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="group">
+        <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
-        <div className="row">
-        <div className="col"></div>
-        <div className="col-8">Nombre Items</div>     "Nombre"
-        <div className="col-3">Botones</div>           "Botones"
-          </div>
-          </div>     
+    </div>
   )
 }
 
-export default App;
+export default App
