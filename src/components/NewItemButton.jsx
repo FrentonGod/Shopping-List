3f661dcfa9d457f4ab8357b7a79e871565942de0
FrentonGod <darianreyesromero@hotmail.com>
2023-04-21 10:01:31 -0600

    Se agregó SweetAlert2 dentro del componente NewItemButton.jsx

--- src/components/NewItemButton.jsx
+++ src/components/NewItemButton.jsx
@@ -1,4 +1,9 @@
+import Swal from "sweetalert2"
+
 const NewItemButton = () => {
+    const newItemModal = () => {
+        Swal.fire('Silksong se acerca')
+    }
     return(
         <button type="button" className="btn btn-outline-primary">
             <i className="bi bi-plus-circle"></i>
